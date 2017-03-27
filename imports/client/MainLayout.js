c9155e0568e561c645dca32a69024270329f717a
Simon Vuong <simon.vuong@yahoo.com>
2017-03-27 00:35:01 -0400

    added mapp. added remaining basic form fields

--- imports/client/MainLayout.js
+++ imports/client/MainLayout.js
@@ -5,12 +5,14 @@ import NotificationForm from './NotificationForm';
 
 function MainLayout () {
   return (
-    <Grid padded>
-      <Grid.Column width={4}>
+    <Grid padded style={{height: 'inherit'}}>
+      <Grid.Column width={5}>
         <NotificationForm />
       </Grid.Column>
-      <Grid.Column width={12}>
-        right
+      <Grid.Column width={11}>
+        <iframe width="600" height="450"
+          src="https://www.google.com/maps/embed/v1/directions?origin=place_id:EioxNiBSYWluYm93IERyLCBNYXlzIExhbmRpbmcsIE5KIDA4MzMwLCBVU0E&destination=place_id:ChIJa2teuNymw4kRjUv0voLCbkA&key=AIzaSyCMmu_dSLLqfqhlnGpLyuyQazYaG_m_Qcs"
+        ></iframe>
       </Grid.Column>
     </Grid>
   )
--- imports/client/NotificationForm.js
+++ imports/client/NotificationForm.js
@@ -7,10 +7,8 @@ function NotificationForm () {
   return (
     <Form>
       <Form.Group>
-        <Form.Field>
-          <label>
-            From
-          </label>
+        <Form.Field width={16}>
+          <label>From</label>
           <Autocomplete
             placeholder='Enter starting location'
             onPlaceSelected={(place) => {console.log(place);}}
@@ -18,16 +16,52 @@ function NotificationForm () {
         </Form.Field>
       </Form.Group>
       <Form.Group>
-       <Form.Field
-          control='Input'
-          label='To'
-          type='text'
-          placeholder='Enter destination location'>
+       <Form.Field width={16}>
+          <label>To</label>
+          <Autocomplete
+            placeholder='Enter destination location'
+            onPlaceSelected={(place) => {console.log(place);}}
+            types={['establishment', 'geocode']} />
         </Form.Field>
       </Form.Group>
-
+      Notify when the travel time is below
+      <Form.Group >
+       <Form.Field
+          width={6}
+          control='Input'
+          label='hr'
+          type='number'
+          placeholder='0'>
+        </Form.Field>
+        <Form.Field
+          width={6}
+          control='Input'
+          label='min'
+          type='number'
+          placeholder='0'>
+        </Form.Field>
+      </Form.Group>
+      Notification method
+      <Form.Group >
+       <Form.Field
+          control='Input'
+          label='Email'
+          width={16}
+          type='Email'
+          placeholder='you@example.com'>
+        </Form.Field>
+      </Form.Group>
+      <Form.Group>
+        <Form.Field
+          control='Input'
+          label='Phone'
+          width={16}
+          type='text'
+          placeholder='(xxx) xxx-xxxx'>
+        </Form.Field>
+      </Form.Group>
       {/*type=button to prevent submitting*/}
-      <Button type='button'>Submit</Button>
+      <Button type='button' primary>Set notification</Button>
     </Form>
   )
 }
